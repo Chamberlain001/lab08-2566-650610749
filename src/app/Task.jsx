37742f84a8b11c72fc164d75a403745c18322889
Kitpakorn <kitpakorn.chopper@gmail.com>
2023-07-17 16:43:40 +0700

    Edited

--- src/app/Task.jsx
+++ src/app/Task.jsx
@@ -2,7 +2,7 @@ export const Task = (props) => {
   return (
     <div className="d-flex p-3 gap-2 align-items-center border-bottom ">
       <li>
-        <span> {props.Task} </span>
+        <span> {props.title} </span>
         <button className="btn btn-success">Done</button>
         <button className="btn btn-danger">Delete</button>
       </li>
