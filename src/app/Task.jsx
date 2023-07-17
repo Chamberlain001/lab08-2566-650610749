e7ba63cbdc2b61fb76f5b0eabc1281a5efc5c20d
Kitpakorn <kitpakorn.chopper@gmail.com>
2023-07-17 16:43:22 +0700

    Edited

--- src/app/Task.jsx
+++ src/app/Task.jsx
@@ -1,13 +1,11 @@
-export const Task = (title) => {
+export const Task = (props) => {
   return (
     <div className="d-flex p-3 gap-2 align-items-center border-bottom ">
-      {title.map((task) => (
-        <li>
-          <span> {title.task} </span>
-          <button className="btn btn-success">Done</button>
-          <button className="btn btn-danger">Delete</button>
-        </li>
-      ))}
+      <li>
+        <span> {props.Task} </span>
+        <button className="btn btn-success">Done</button>
+        <button className="btn btn-danger">Delete</button>
+      </li>
     </div>
   );
 };
