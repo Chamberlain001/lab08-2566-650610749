bc6fa4855144e66b922e4b2c7726bc6bd8e28468
Kitpakorn <kitpakorn.chopper@gmail.com>
2023-07-17 16:15:30 +0700

    Edited

--- src/app/Header.jsx
+++ src/app/Header.jsx
@@ -1,8 +1,10 @@
 export const Header = () => {
   return (
     <div>
-      <h4>Todo List Mock Up</h4>
-      <p>This lab teach you how to split react component(s)</p>
+      <h4 className="display-4 fst-italic text-center">Todo List Mock Up</h4>
+      <p className="text-secondary text-center">
+        This lab teach you how to split react component(s)
+      </p>
     </div>
   );
 };
