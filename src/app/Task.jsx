0e6dfa4034938fd22fca5b5e495b5be0468876e3
Kitpakorn <kitpakorn.chopper@gmail.com>
2023-07-17 16:27:45 +0700

    Edited

--- src/app/Task.jsx
+++ src/app/Task.jsx
@@ -2,13 +2,6 @@ export const Task = () => {
   const tasks = ["Read a book", "Take a shower", "Sleep"];
   return (
     <div>
-      //{" "}
-      <div className="d-flex p-3 gap-2 align-items-center border-bottom">
-        // <span>Read a book</span>
-        // <button className="btn btn-success">Done</button>
-        // <button className="btn btn-danger">Delete</button>
-        //{" "}
-      </div>
       {tasks.map((task, i) => (
         <li key={i}>
           <span> {task} </span>
